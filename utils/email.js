b64879f40ec985a070b9db46248b6a7fb3df3cd4
Est-Wa <107359113+Est-Wa@users.noreply.github.com>
2023-03-16 14:12:46 +0200

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -4,15 +4,12 @@ const sendingMail = async (member_mail,subject,text) => {
         service: 'outlook',
         auth: {
             //user: process.env.EMAIL_ADDRESS,
-            user: '36326151263@mby.co.il',
             //pass: process.env.EMAIL_PASSWORD,
-            pass: 'Student@264'
         }
     });
 
     var mailOptions = {
         //from: process.env.EMAIL_ADDRESS,
-        from: '36326151263@mby.co.il',
         to: member_mail,
         subject,
         text,
